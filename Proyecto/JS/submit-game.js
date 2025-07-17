a1b27ebcf87f03907eaaf373095f7ce1291d5529
DarkSlayerV201 <lucasvilchekapo@gmail.com>
2025-07-17 18:20:36 -0300

    Add files via upload

--- Proyecto/JS/submit-game.js
+++ Proyecto/JS/submit-game.js
@@ -23,26 +23,48 @@ function crearParrafoMensaje(contenidoMensaje, selectorPadre) {
 let peso = document.getElementById("peso");
 let AchicarPeso = "Se puede hacer que pese menos?".concat(preguntarSiNo());
 let PartirJuego = "Se puede partir el juego en paquetes instalables para reducir espacio?".concat(preguntarSiNo());
-let labelAchicarPeso = crearLabel(AchicarPeso);
+let labelAchicarPeso = crearLabel(AchicarPeso.concat(preguntarSiNo()));
 let labelPartirJuego = crearLabel(PartirJuego);
 let restriccionEdad = document.getElementById("restriccionEdad");
 
 // Condicionales
-if (peso > 10 && peso < 100) {
+peso.addEventListener("keypress", function () {
+    if (peso > 10 && peso < 100) {
     document.getElementById("form_box").appendChild(labelAchicarPeso);
     document.getElementById("form_box").appendChild(labelPartirJuego);
-} else if (peso > 100 && peso < 1000) {
+    } else {
+        let descripcionAEliminar = document.getElementById("mensaje");
+        descripcionAEliminar.remove();
+        if (peso > 100 && peso < 1000) {
+            let divContenedor = document.createElement("div");
+            divContenedor.innerHTML = "<p>En principio, no aceptamos juegos de más de 100MB (eso se puede guardar para el futuro 1GB Games). Sin embargo, si así todo pensas que tu juego cumple con la idea de ser un juego que todos puedan jugarlo,podes detallar un poco por qué pensas que es así</p><textarea name='message' rows='2' placeholder='Aca tenes solo 200 caracteres' maxlength='200'></textarea>";
+            document.getElementById("form_box").appendChild(divContenedor);
+        } else if (peso > 1000) {
+            let parrafoNegativo = "No aceptamos juegos de más de 1GB, eso va en el futuro 1GB Games. Por ahora, acortá el peso"
+            crearParrafoMensaje(parrafoNegativo, "form_box")
+        } else if (peso < 0) {
+            let parrafoMenorA0 = "No";
+            crearParrafoMensaje(parrafoMenorA0, "form_box");
+        };
+    };
+});
+
+/*if (peso > 10 && peso < 100) {
+    document.getElementById("form_box").appendChild(labelAchicarPeso);
+    document.getElementById("form_box").appendChild(labelPartirJuego);
+} else {
     let descripcionAEliminar = document.getElementById("mensaje");
     descripcionAEliminar.remove();
-    let divContenedor = document.createElement("div");
-    divContenedor.innerHTML = "<p>En principio, no aceptamos juegos de más de 100MB (eso se puede guardar para el futuro 1GB Games). Sin embargo, si así todo pensas que tu juego cumple con la idea de ser un juego que todos puedan jugarlo,podes detallar un poco por qué pensas que es así</p><textarea name='message' rows='2' placeholder='Aca tenes solo 200 caracteres' maxlength='200'></textarea>";
-    document.getElementById("form_box").appendChild(divContenedor);
-} else if (peso > 1000) {
-    document.getElementById("mensaje").remove(); //Acá estamos tratando de eliminar un 'textarea' directamente, sin antes asignarle una variable
-    let parrafoNegativo = "No aceptamos juegos de más de 1GB, eso va en el futuro 1GB Games. Por ahora, acortá el peso"
-    crearParrafoMensaje(parrafoNegativo, "form_box")
-} else if (peso < 0) {
-    document.getElementById("mensaje").remove();
-    let parrafoMenorA0 = "No";
-    crearParrafoMensaje(parrafoMenorA0, "form_box");
-}
+    if (peso > 100 && peso < 1000) {
+        let divContenedor = document.createElement("div");
+        divContenedor.innerHTML = "<p>En principio, no aceptamos juegos de más de 100MB (eso se puede guardar para el futuro 1GB Games). Sin embargo, si así todo pensas que tu juego cumple con la idea de ser un juego que todos puedan jugarlo,podes detallar un poco por qué pensas que es así</p><textarea name='message' rows='2' placeholder='Aca tenes solo 200 caracteres' maxlength='200'></textarea>";
+        document.getElementById("form_box").appendChild(divContenedor);
+    } else if (peso > 1000) {
+        let parrafoNegativo = "No aceptamos juegos de más de 1GB, eso va en el futuro 1GB Games. Por ahora, acortá el peso"
+        crearParrafoMensaje(parrafoNegativo, "form_box")
+    } else if (peso < 0) {
+        let parrafoMenorA0 = "No";
+        crearParrafoMensaje(parrafoMenorA0, "form_box");
+    }
+}*/
+
