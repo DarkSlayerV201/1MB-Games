be82a3956174180290ecdbec13e09e5f0bbb8e7b
DarkSlayerV201 <lucasvilchekapo@gmail.com>
2025-07-30 14:50:00 -0300

    Add files via upload

--- Proyecto/JS/submit-game.js
+++ Proyecto/JS/submit-game.js
@@ -18,6 +18,10 @@ let camposValidos = {
     plataformaEsValida: 0
 }
 
+let selectoresValidos = {
+    reducibleEsValido: 0,
+    partibleEsValido: 0
+}
 
 // Funciones de inserción de HTML //
 
@@ -52,7 +56,7 @@ function agregarMensajeInvalido(contenidoInvalido, idInvalido) {
     parrafoInvalido.textContent = contenidoInvalido
 
     divPadre.appendChild(parrafoInvalido)
-    document.getElementById("form_box").appendChild(divPadre)
+    document.getElementById("divErrores").appendChild(divPadre)
 }
 
 function crearSelector(labelPadre, idSelector) {
@@ -100,7 +104,7 @@ function preguntarPesoReducible() {
 function pedirEdadRecomendada () {
     let labelPadre = document.createElement("label")
     labelPadre.id = "edadRecomendada"
-    labelPadre.innerHTML = 'Edad recomendada:<input type="number" name="edadRecomendada" placeholder="1" id="edadRecomendadaNumero">'
+    labelPadre.innerHTML = 'Edad recomendada:<input type="number" name="edadRecomendada" placeholder="1" id="edadRecomendadaNumero" max="100">'
     document.getElementById("form_box").appendChild(labelPadre)
 }
 
@@ -114,6 +118,7 @@ function eliminarMensajesDePeso() {
     try {document.getElementById("labelAgregado2").remove()} catch (error) {}
     try {document.getElementById("selectorMenorPesoInvalido").remove()} catch (error) {}
     try {document.getElementById("selectorPartirJuegoInvalido").remove()} catch (error) {}
+    try {document.getElementById("justifiacionPesoMayorA100Invalido").remove()} catch (error) {}
 }
 
 function eliminarMensajeUnico(idMensaje) {
@@ -166,15 +171,15 @@ function validarSelector(campo, idCampo, claveCampo, agregarEvento = false) {
 
     eliminarMensajeUnico(idCampo.concat("Invalido"))
 
-    //valor.addEventListener("change", function() {
-        if (valor.value == "opcion_placeholder") {
-            agregarMensajeInvalido(campo.concat(' falta elegir una opción válida'), idCampo.concat("Invalido"))
-            camposValidos[claveCampo] = 0
-        } else {camposValidos[claveCampo] = 1}
-        console.log(campo.concat(' esValido: ').concat(camposValidos[claveCampo]))
+    
+    if (valor.value == "opcion_placeholder") {
+        agregarMensajeInvalido(campo.concat(' falta elegir una opción válida'), idCampo.concat("Invalido"))
+        camposValidos[claveCampo] = 0
+    } else {camposValidos[claveCampo] = 1}
+    console.log(campo.concat(' esValido: ').concat(camposValidos[claveCampo]))
 
-        validarCampos()
-    //})
+    validarCampos()
+    
 
     if (agregarEvento) {
         valor.addEventListener("change", function() {
@@ -183,22 +188,54 @@ function validarSelector(campo, idCampo, claveCampo, agregarEvento = false) {
     }
 }
 
+function validarSelectoresDePeso(campo, idCampo, claveCampo, agregarEvento = false) {
+    let valor = document.getElementById(idCampo)
+
+    eliminarMensajeUnico(idCampo.concat("Invalido"))
+
+    
+    if (valor.value == "opcion_placeholder") {
+        agregarMensajeInvalido(campo.concat(' falta elegir una opción válida'), idCampo.concat("Invalido"))
+        selectoresValidos[claveCampo] = 0
+    } else {selectoresValidos[claveCampo] = 1}
+    console.log(campo.concat(' esValido: ').concat(selectoresValidos[claveCampo]))
+
+    validarMultiplesSelectores()
+    validarCampos()
+    
+
+    if (agregarEvento) {
+        valor.addEventListener("change", function() {
+            validarSelectoresDePeso(campo, idCampo, claveCampo)
+        })
+    }
+}
+
+function validarMultiplesSelectores() {
+    let selectorEsValido = Object.values(selectoresValidos).reduce((a, b) => a + b, 0)
+
+    if (selectorEsValido == 2) {camposValidos["pesoEsValido"] = 1}
+    else {camposValidos["pesoEsValido"] = 0}
+}
+
 function validarCampos() { 
     // esValido tiene que ser igual a la suma de los campos para resultar validado
     // Si la validacion de todos los campos es igual a 1, entonces 'esValido' sera igual a 6/
+
     let esValido = Object.values(camposValidos).reduce((a, b) => a + b, 0)
     console.log ("esValido = " + esValido)
 
     eliminarMensajeUnico("divAgregadoFinal")
     eliminarBotonEnviar()
 
-    if (esValido == 7) {agregarBotonEnviar()} else {agregarMensajeInvalido('Todos los campos deben estar rellenados', "divAgregadoFinal")}
+    if (esValido == 7) {agregarBotonEnviar()} 
+    else {agregarMensajeInvalido('Todos los campos deben estar rellenados', "divAgregadoFinal")}
 }
 
 
 // Eventos //
 
-peso.addEventListener("keyup", function() {
+peso.addEventListener("keyup", function() { //Funciona correctamente
     eliminarMensajesDePeso()
 
     if (peso.value <= 0) {
@@ -208,13 +245,13 @@ peso.addEventListener("keyup", function() {
 
         if (peso.value <= 10) {camposValidos["pesoEsValido"] = 1}
         else if (peso.value > 10 && peso.value <= 100) {
-            preguntarPesoReducible()
             camposValidos["pesoEsValido"] = 0
-            validarSelector('En peso reducible', "selectorMenorPeso", "pesoEsValido", true)
-            validarSelector('En peso partible', "selectorPartirJuego", "pesoEsValido", true)
-            //camposValidos["pesoEsValido"] = 1
+            preguntarPesoReducible()
+            validarSelectoresDePeso('En peso reducible', "selectorMenorPeso", "reducibleEsValido", true)
+            validarSelectoresDePeso('En peso partible', "selectorPartirJuego", "partibleEsValido", true)
         }
         else if (peso.value > 100) {
+            camposValidos["pesoEsValido"] = 0
             agregarMensajePesoMayorA100()
             validarString('La justificación', "justifiacionPesoMayorA100", 'pesoEsValido')
         }
@@ -225,61 +262,47 @@ peso.addEventListener("keyup", function() {
     validarCampos()
 })
 
-restriccionEdad.addEventListener("change", function() {
+restriccionEdad.addEventListener("change", function() { //Funciona correctamente
     eliminarMensajeUnico("edadRecomendada")
     eliminarMensajeUnico("edadRecomendadaNumeroInvalido")
+    eliminarMensajeUnico("restriccionEdadInvalido")
 
     if (restriccionEdad.value == "no") {
         camposValidos["restriccionEdadEsValido"] = 0
         pedirEdadRecomendada()
-
-        let edadRecomendada = document.getElementById("edadRecomendada")
-        edadRecomendada.addEventListener("keyup", function() {
-            validarFloat('Edad recomendada', 'edadRecomendadaNumero', "restriccionEdadEsValido")
-            //console.log("edadRecomendada fue triggereado")
-            //console.log("restriccionEdadEsValido = " + restriccionEdadEsValido)
-            validarCampos()
-        })
-        //validarFloat('Edad recomendada', 'edadRecomendadaNumero', "restriccionEdadEsValido")
-        //console.log("edadRecomendada.value" + edadRecomendada.value)
+        validarFloat('Edad recomendada', 'edadRecomendadaNumero', "restriccionEdadEsValido")
     } else {
+        camposValidos["restriccionEdadEsValido"] = 0
         eliminarMensajeUnico()
-        validarSelector('en restricción de edad', "restriccionEdad", "restriccionEdadEsValido")
+        validarSelector('En restricción de edad', "restriccionEdad", "restriccionEdadEsValido")
     }
 
-    //console.log("restriccionEdad.value = " + restriccionEdad.value)
-    //console.log("restriccionEdadEsValido = " + restriccionEdadEsValido)
     validarCampos()
 })
 
-nombre.addEventListener("keyup", function() {
+nombre.addEventListener("keyup", function() {  //Funciona correctamente
     eliminarMensajeUnico("divAgregadoNombre")
 
     if (nombre.value.trim() == "") {
         agregarMensajeInvalido('El nombre no puede quedar vacio', "divAgregadoNombre")
-        //camposValidos[nombreEsValido] = 0
         camposValidos["nombreEsValido"] = 0
-    }
-    else {camposValidos["nombreEsValido"] = 1}
+    } else {camposValidos["nombreEsValido"] = 1}
 
-    //console.log("nombreEsValido = " + nombreEsValido)
     validarCampos()
 })
 
-apodo.addEventListener("keyup", function() {
+apodo.addEventListener("keyup", function() {  //Funciona correctamente
     eliminarMensajeUnico("divAgregadoApodo")
 
     if (apodo.value.trim() == "") {
         agregarMensajeInvalido('El apodo no puede quedar vacio', "divAgregadoApodo")
         camposValidos["apodoEsValido"] = 0
-    }
-    else {camposValidos["apodoEsValido"] = 1}
+    } else {camposValidos["apodoEsValido"] = 1}
 
-    //console.log("apodoEsValido = " + apodoEsValido)
     validarCampos()
 })
 
-mensaje.addEventListener("keyup", function() {
+mensaje.addEventListener("keyup", function() {  //Funciona correctamente
     eliminarMensajeUnico("divAgregadoMensaje")
 
     if (mensaje.value.trim() == "") {
@@ -287,11 +310,10 @@ mensaje.addEventListener("keyup", function() {
         camposValidos["mensajeEsValido"] = 0
     } else {camposValidos["mensajeEsValido"] = 1}
 
-    //console.log("mensajeEsValido = " + mensajeEsValido)
     validarCampos()
 })
 
-email.addEventListener("keyup", function() {
+email.addEventListener("keyup", function() {  //Funciona correctamente
     eliminarMensajeUnico("divAgregadoEmail")
 
     if (email.value.trim() == "") {
@@ -299,11 +321,10 @@ email.addEventListener("keyup", function() {
         camposValidos["emailEsValido"] = 0
     } else {camposValidos["emailEsValido"] = 1}
 
-    //console.log("emailEsValido = " + emailEsValido)
     validarCampos()
 })
 
-plataforma.addEventListener("change", function() {
+plataforma.addEventListener("change", function() { //Funciona correctamente
     eliminarMensajeUnico("divAgregadoPlataforma")
 
     validarSelector('En plataforma', "plataforma", "plataformaEsValida")
